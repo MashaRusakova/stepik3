65d38948a52e1976cb7e5673a4f5085844a3bffa
MashaRusakova <masharusakova87@mail.ru>
2020-07-26 21:52:58 +0300

    commit2

--- drf_simple/drf_simple/views.py
+++ drf_simple/drf_simple/views.py
@@ -9,31 +9,33 @@ from rest_framework.request import Request
 def beautybox_list(request: Request) -> Response:
     response = requests.get('https://stepik.org/media/attachments/course/73594/beautyboxes.json')
     result = []
-    listdata = {}
 
     beautyboxes = response.json()
 
-    # for box in beautyboxes:
-    #     listdata.update(box['name'])
-    #     listdata.update(box['about'])
-    #     result.append(listdata)
-    #     listdata = {}
-    # beautyboxes = result
-
     if request.query_params:
-        price = request.query_params.get('min_price')
-        weight = request.query_params.get('min_weight')
-        if not price or weight:
+        price = request.query_params.get('min_price', 0)
+        weight = request.query_params.get('min_weight', 0)
+
+        if not (price or weight):
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
         for box in beautyboxes:
-            if box['price'] >= int(price) or box['weight'] >= int(weight):
+            if box['price'] >= int(price) or box['weight_grams'] >= int(weight):
                 result.append(box)
 
     else:
         result = beautyboxes
 
-    return Response(result)
+    result_formatted = []
+    for box in result:
+        listdata = {}
+        listdata['name'] = box.get('name')
+        listdata['about'] = box.get('about')
+        listdata['weight_grams'] = box.get('weight_grams')
+        listdata['price'] = box.get('price')
+        result_formatted.append(listdata)
+
+    return Response(result_formatted)
 
 
 @api_view(http_method_names=['GET'])
@@ -44,9 +46,15 @@ def beautybox_content(request: Request, pk: int) -> Response:
     for beautybox_item in beautyboxes:
         if beautybox_item['inner_id'] == pk:
             response = beautybox_item
+            listdata = {}
+            listdata['name'] = beautybox_item.get('name')
+            listdata['about'] = beautybox_item.get('about')
+            listdata['weight_grams'] = beautybox_item.get('weight_grams')
+            listdata['price'] = beautybox_item.get('price')
+        beautyboxes = listdata
 
     if response:
-        return Response(response)
+        return Response(beautyboxes)
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
